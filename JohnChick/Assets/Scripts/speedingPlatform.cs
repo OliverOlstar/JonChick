d14ed7b9c1ac8cbfbad7c9ffc6ec0b9f5eea0c23
Alvys34 <iznopgia91@gmail.com>
2019-08-08 17:28:37 -0400

    Speeding up platform prefab + script

---
+++ JohnChick/Assets/Scripts/speedingPlatform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script for the speeding platforms 
+public class speedingPlatform : MonoBehaviour
+{
+    public Vector3 direction;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction.Normalize();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        other.gameObject.GetComponent<Rigidbody>().AddForce(direction * speed); //add force in a direction
+            
+    }
+}
