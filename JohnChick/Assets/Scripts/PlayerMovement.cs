26a040ae43a51bd60da5445d14d59ba2fac1440c
Oliver Loescher <48457872+Olstar72@users.noreply.github.com>
2019-08-08 14:56:21 -0400

    Movement

---
+++ JohnChick/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float maxSpeed = 5f;
+
+    private Rigidbody _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveInput = moveInput.normalized * moveSpeed * Time.deltaTime;
+
+        if (_rb.velocity.magnitude < maxSpeed)
+            _rb.AddForce(moveInput);
+    }
+}
